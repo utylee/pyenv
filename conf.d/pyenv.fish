e4453795159ec3b8565f440dc5657675af21a393
utylee <utylee@gmail.com>
2021-11-06 01:48:12 +0900

    fix

--- conf.d/pyenv.fish
+++ conf.d/pyenv.fish
@@ -3,19 +3,19 @@ if status is-interactive
 		echo "Install <github.com/yyuu/pyenv> to use 'pyenv'."
 		exit 1
 	end
-end
 
-set -l pyenv_root ""
+	set -l pyenv_root ""
 
-if test -z "$PYENV_ROOT"
-    set pyenv_root ~/.pyenv
-    set -x PYENV_ROOT "$pyenv_root"
-else
-    set pyenv_root "$PYENV_ROOT"
-end
+	if test -z "$PYENV_ROOT"
+		set pyenv_root ~/.pyenv
+		set -x PYENV_ROOT "$pyenv_root"
+	else
+		set pyenv_root "$PYENV_ROOT"
+	end
 
-if status --is-login
-    set -x PATH "$pyenv_root/shims" $PATH
-    set -x PYENV_SHELL fish
+	if status --is-login
+		set -x PATH "$pyenv_root/shims" $PATH
+		set -x PYENV_SHELL fish
+	end
+	command mkdir -p "$pyenv_root/"{shims,versions}
 end
-command mkdir -p "$pyenv_root/"{shims,versions}
